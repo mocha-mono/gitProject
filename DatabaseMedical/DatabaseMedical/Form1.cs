6006d2842d2d5f4ace134b26dad4b8dc4980bf22
mitch <mccprfsnl@gmail.com>
2024-04-21 12:50:45 +0800

    save progress testing

--- DatabaseMedical/DatabaseMedical/Form1.cs
+++ DatabaseMedical/DatabaseMedical/Form1.cs
@@ -12,9 +12,46 @@ namespace DatabaseMedical
 {
     public partial class Form1 : Form
     {
+        BindingSource patientsBindingSource = new BindingSource();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PatientsDAO x = new PatientsDAO();
+            Patient a1 = new Patient
+            {
+                ID = 1,
+                LastName = "Cabarrubias",
+                FirstName = "Mitchel",
+                MiddleName = "Cantano",
+                SuffixName = "N/A",
+                BirthDate = DateTime.Now,
+                Address = "Camp 7"
+
+            };
+
+            Patient a2 = new Patient
+            {
+                ID = 1,
+                LastName = "Klein",
+                FirstName = "Anne",
+                MiddleName = "Eeep",
+                SuffixName = "N/A",
+                BirthDate = DateTime.Now,
+                Address = "Japan"
+
+            };
+
+            x.patients.Add(a1);
+            x.patients.Add(a2);
+
+            // list connect
+            patientsBindingSource.DataSource = x.patients;
+            dataGridView1.DataSource = patientsBindingSource;
+
         }
     }
 }
--- DatabaseMedical/DatabaseMedical/Program.cs
+++ DatabaseMedical/DatabaseMedical/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,5 +19,18 @@ namespace DatabaseMedical
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+    }
+
+    internal class Patient 
+    {
+        public int ID { get; set; }
+        public String LastName { get; set; }
+        public String FirstName { get; set; }
+        public String MiddleName { get; set; }
+        public String SuffixName {  get; set; }
+        public DateTime BirthDate { get; set; }
+        public String Address { get; set; }
+        
+        // make List<Patient> 
     }
 }
